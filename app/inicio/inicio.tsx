7c31dbf7d23817f1722c23b41f27730d309e7e75
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-30 00:43:17 -0300

    Adicionada função para comunicação entre a pagina inicial e campos de conversão

--- app/inicio/inicio.tsx
+++ app/inicio/inicio.tsx
@@ -1,6 +1,24 @@
+import {useEffect, useState} from 'react';
 import { CampoConversao } from "./components/CampoConversao";
 
 export function Inicio() {
+  const [valorCampo1, setValorCampo1] = useState("");
+  const [valorCampo2, setValorCampo2] = useState("");
+
+  function handlerValorCampo1(valor: string, siglaMoeda: string) {
+    setValorCampo1(valor);
+  }
+
+  function handlerValorCampo2(valor: string, siglaMoeda: string) {
+    setValorCampo2(valor);
+  }
+
+  useEffect(() => {
+    if (valorCampo1 || valorCampo2) {
+      console.log(`Valor Campo 1: ${valorCampo1}, Valor Campo 2: ${valorCampo2}`);
+    }
+  }, [valorCampo1, valorCampo2]);
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -16,13 +34,13 @@ export function Inicio() {
         </div>
         <div className="grid grid-cols-5 w-full max-w-[800px] gap-4">
             <div className="col-span-2">
-              <CampoConversao />
+              <CampoConversao onEnviaValorMoedaCampo={handlerValorCampo1} />
             </div>
             <div className="col-span-1 my-auto">
               <img src="/image/convert.png" alt="Icone de Conversão" className="h-6 w-6 mx-auto" />
             </div>
             <div className="col-span-2">
-              <CampoConversao />
+              <CampoConversao onEnviaValorMoedaCampo={handlerValorCampo2}/>
             </div>
         </div>
         
