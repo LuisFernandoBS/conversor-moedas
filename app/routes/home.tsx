3220f8e2fad2851c0e7741ee1e304720d17f8ca1
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-29 23:20:57 -0300

    Alterações da home para utilização da pagina inicial

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Inicio } from "../inicio/inicio";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Conversor de Moedas" },
+    { name: "description", content: "Inicio" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Inicio />;
 }
