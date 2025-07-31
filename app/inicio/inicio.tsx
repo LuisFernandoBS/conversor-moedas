2c5dcde8556436e107358ed817245eecdce63cbd
LuisFernandoBS <luisfernando142536@gmail.com>
2025-07-30 21:38:33 -0300

    Implementada animação na img após conversão

--- app/inicio/inicio.tsx
+++ app/inicio/inicio.tsx
@@ -20,6 +20,9 @@ export function Inicio() {
 
   const [listaTaxasConsultadas, setListaTaxasConsultadas] = useState<ArrayTaxasCambio[]>([]);
   const [siglaUltTaxa, setSiglaUltTaxa] = useState('');
+  
+  const [animacao, setAnimacao] = useState(false);
+
 
   useEffect(()=>{
     if(valorCampo1 || valorCampo2) {
@@ -33,6 +36,7 @@ export function Inicio() {
     if(taxa.length > 0){
       const valorConvertido = parseFloat(valorFloat) * taxa[0].taxas[moedaCampo2];
       setValorCampo2(valorConvertido.toFixed(2));
+      iniciarAnimacao();
     }
   }
 
@@ -63,6 +67,13 @@ export function Inicio() {
     });
   }
 
+  function iniciarAnimacao() {
+    setAnimacao(true);
+    setTimeout(() => {
+      setAnimacao(false);
+    }, 1300);
+  }
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -81,7 +92,7 @@ export function Inicio() {
               <CampoConversao id="1" moeda={moedaCampo1} valorInput={valorCampo1} setMoeda={setMoedaCampo1} setValor={setValorCampo1} />
             </div>
             <div className="col-span-1 my-auto">
-              <img src="/image/convert.png" alt="Icone de Conversão" className="h-6 w-6 mx-auto" />
+              <img src="/image/convert.png" alt="Icone de Conversão" className={`h-6 w-6 mx-auto ${animacao && 'animacao-rotacao'}`} />
             </div>
             <div className="col-span-2">
               <CampoConversao id="2" moeda={moedaCampo2} valorInput={valorCampo2} setMoeda={setMoedaCampo2} setValor={setValorCampo2} />
